49c3bb2b0623b5be7c3aeffef40bd50c546ba494
plubber <er.h.gorski@gmail.com>
2022-06-02 17:02:40 -0400

    cleanup

--- app/export/tfm/contract_export_tfm_farm.go
+++ app/export/tfm/contract_export_tfm_farm.go
@@ -18,13 +18,13 @@ func ExportTfmFarms(app *terra.TerraApp, bl util.Blacklist) (util.SnapshotBalanc
 	qs := util.PrepWasmQueryServer(app)
 	logger.Info("Exporting TFM farms")
 
-	for _, farm := range FarmContracts {
-		farmAddr, err := sdk.AccAddressFromBech32(farm)
+	for _, farmAddr := range FarmContracts {
+		farmAddrAccAddr, err := sdk.AccAddressFromBech32(farmAddr)
 		if err != nil {
 			return nil, err
 		}
 		prefix := util.GeneratePrefix("reward")
-		app.WasmKeeper.IterateContractStateWithPrefix(sdk.UnwrapSDKContext(ctx), farmAddr, prefix, func(key, value []byte) bool {
+		app.WasmKeeper.IterateContractStateWithPrefix(sdk.UnwrapSDKContext(ctx), farmAddrAccAddr, prefix, func(key, value []byte) bool {
 
 			userAddr := string(key)
 			stakerUstBal := getStakerUstBal(ctx, qs, userAddr, farmAddr)
@@ -40,7 +40,7 @@ func ExportTfmFarms(app *terra.TerraApp, bl util.Blacklist) (util.SnapshotBalanc
 	return snapshot, nil
 }
 
-func getStakerUstBal(ctx context.Context, q types.QueryServer, userAddr string, farmAddr sdk.AccAddress) sdk.Int {
+func getStakerUstBal(ctx context.Context, q types.QueryServer, userAddr string, farmAddr string) sdk.Int {
 	USTBalance := sdk.NewInt(0)
 	var info stakerInfo
 	if err := util.ContractQuery(ctx, q, &wasmtypes.QueryContractStoreRequest{
